310e8a4127fad7fa4535a172a3c52dafa28a1f25
Andrew Pellum <apellum19@gmail.com>
2021-06-14 15:01:10 -0600

    commit tree b6c2e68304bba4b6da426558d4c9fc3d20ef8b59

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+const findMatching = (drivers, name) => {
+    const newDrivers = drivers.filter(function (person) {
+        return person.toLowerCase() === name.toLowerCase()
+    });
+    return newDrivers
+} 
+
+const fuzzyMatch = (drivers, query) => {
+        return drivers.filter(function(firstLetters) {
+            return firstLetters.toLowerCase().indexOf(query.toLowerCase()) === 0
+        })
+      }
+    
+//const matchName = (collection, string) => {
+
+
+    /*const length = query.length;
+    return drivers.filter(el => el.toLowerCase().indexOf(query.toLowerCase())!== -1)
+    if () {
+    return []
+*/
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+      return driver.name === name
+    })
+  }
